44aa6d5d48bdff345446c0f881932c58f15bf996
marff <Marffirstlove@gmail.com>
2021-10-16 09:18:28 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    
+    <div >
+        <h2 >Front-End Developer</h2>
+        <h1 >Sergey Urakov</h1>
+        <p >Hello. My name is Sergey Urakov and I am FrontEnd developer</p>
+    
+    
+        <h3 >Projects</h3>
+        <ol >
+            <li > 
+                <span >
+                    <a href="http://marffirst.zzz.com.ua/" >http://marffirst.zzz.com.ua/</a> 
+                    ..............
+                    <span >[</span> HTML5, CSS3 <span >]</span>
+                </span>
+            </li>
+            <li > 
+                <span >
+                    <a href="https://cryptohub.goit.global/" >https://cryptohub.goit.global/</a> 
+                    ........
+                    <span >[</span> JavaScript <span >]</span>
+                </span>
+            </li>
+            <li > 
+                <span >
+                    <a href="https://kidslike.goit.global/" >https://kidslike.goit.global/</a> 
+                    ...............
+                    <span >[</span> React.js, Node.js <span >]</span>
+                </span>
+            </li>
+        </ol>
+    </div>
+    
+
+  );
+}
+
+export default App;
